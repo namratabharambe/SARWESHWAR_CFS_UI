01a20dd293533b26ac912e7d4f0b9613dcfc4874
User-6\Namrata <namrata.bharambe@prosperinfotech.com>
2026-09-23 12:44:21 +0530

    Configure local dev proxy for CORS bypass to Prosper cloud endpoints

--- cfs-admin-UI/cfs-UI/src/environments/environment.ts
+++ cfs-admin-UI/cfs-UI/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://cfsapi.prosperassettracking.com/api',
-  gateApiBaseUrl: 'https://syapi.prosperassettracking.com/api/v1',
+  apiBaseUrl: '/api',
+  gateApiBaseUrl: '/api/v1',
 };
 
